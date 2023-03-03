5d011ca410a758d445dee94b3a0f90f880de0b58
garry911128 <garry80633@gmail.com>
2023-03-03 21:42:38 +0800

    03/3

---
+++ Source/Game/Map.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace game_framework {
+	class Map { //地圖的物件(格子)
+	public:
+		Map(vector<vector<int>> stage); //要傳某一個stage的每個格子 type
+		vector<vector<MapItem>> _Stage; //每一個stage
+		int _BoardX; //棋盤的x
+		int _BoardY; //棋盤的y
+		void OnShow(); 
+	};
+}
---
+++ Source/Game/Menu.cpp
@@ -0,0 +1,59 @@
+#include "stdafx.h"
+#include "../Core/Resource.h"
+#include <string>
+#include <mmsystem.h>
+#include <ddraw.h>
+#include "../Library/audio.h"
+#include "../Library/gameutil.h"
+#include "../Library/gamecore.h"
+#include "./Menu.h"
+#include <string>
+
+using namespace game_framework;
+
+Menu::Menu() {
+	_TotalScore = 0;
+	_TheHightestScore = 0;
+	tempselect = 0;
+}
+
+void Menu::LoadBitmap() {
+	_Menu.LoadBitmapByString({ "resources/LobbyMenu.bmp" });
+	_Menu.SetTopLeft(0, 0);
+	_Arrow.LoadBitmapByString({ "resources/LobbyArrow.bmp" });
+	_Arrow.SetTopLeft(280, 500);
+}
+
+void Menu::OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags) {
+	const char KEY_UP = 0x26;
+	const char KEY_DOWN = 0x28;
+	const char KEY_ENTER = 0x0D;
+
+	if (nChar == KEY_DOWN) {
+		tempselect += 1;
+		tempselect %= 2;
+	}
+	else if (nChar == KEY_UP) {
+		tempselect -= 1;
+		tempselect += 2;
+		tempselect %= 2;
+	}
+	else if (nChar == KEY_ENTER) {
+		return;
+	}
+	_Arrow.SetTopLeft(280, 500 + 70 * tempselect);
+
+}
+
+void Menu::OnShow() {
+	_Menu.ShowBitmap();
+	_Arrow.ShowBitmap();
+}
+void Menu::OnShowText(CDC *pDC, CFont* &fp) {
+
+	pDC->SetBkMode(TRANSPARENT);
+	pDC->SetTextColor(RGB(180, 180, 180));
+	CTextDraw::ChangeFontLog(pDC, 40, "STZhongsong", RGB(180, 180, 180));
+	CTextDraw::Print(pDC, 200, 60, to_string(_TotalScore).c_str());
+
+}
