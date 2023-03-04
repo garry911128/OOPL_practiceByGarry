744d46bc791684a05269fa97f3916689182c9226
Gen-Yan Chen <t110590048@ntut.edu.tw>
2023-03-04 16:54:50 +0800

    03/04

---
+++ Source/Game/Map.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "MapItem.h"
+namespace game_framework {
+	class Map { //地圖的物件(格子)
+	public:
+		Map();
+		vector<vector<MapItem>> _Stage; //每一個stage
+		void OnInit(vector<vector<int>> stage); //要傳某一個stage的每個格子 type
+		int _BoardX; //棋盤的x
+		int _BoardY; //棋盤的y
+		void OnShow();
+	};
+}
---
+++ Source/Game/MapItem.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+namespace game_framework {
+	class MapItem { //地圖的物件(格子)
+	public:
+		MapItem();
+		MapItem(int Type);
+		int _Type;
+		bool _IfShoot;  //是否可以射擊 false 不行,true可以
+		bool _IfBreak;  //是否可以破壞 false 不行,true可以
+		bool _IfWalk;    //是否可以走上去(格子) false 不行,true可以
+		CMovingBitmap _OneGrid;
+		void OnShow();
+	};
+}
