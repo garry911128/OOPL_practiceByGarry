1b1bea68cf7e042dd23f1de2d29a3d0abaa86a8d
Gen-Yan Chen <t110590048@ntut.edu.tw>
2023-03-10 21:52:34 +0800

    03/10

--- Source/Game/CTank.cpp
+++ Source/Game/CTank.cpp
@@ -12,7 +12,6 @@
 using namespace game_framework;
 
 CTank::CTank() {
-	last_time = clock();
 	_X = 0;
 	_Y = 0;
 	_OriginAngle = 90;
@@ -22,10 +21,11 @@ CTank::CTank() {
 	_LocationDistance = 16;
 	_OffsetX = 0;
 	_OffsetY = 0;
-	_FrameIndex = 0;
+	_FrameTime = 0;
+	_Frameindex = 0;
 	_PointX = _X;
 	_PointY = _Y;
-	_MovementSpeed = 1;
+	_MovementSpeed = 2;
 	_AttackSpeedUP = false;
 	_CanBreakIron = false;
 	_DoubleAttack = false;
@@ -50,66 +50,67 @@ void CTank::SetXY(int _x, int _y) {
 	_Y = _y;
 }
 void CTank::Move(UINT nChar) {
-	if (nChar == VK_RIGHT){
-		_TurnAngle = 90;
+	TurnFace(nChar);
+	if (_OriginAngle == 90) {			//右
 		_OffsetX += _MovementSpeed;
 		_X += _MovementSpeed;
 	}
-	else if (nChar == VK_LEFT){
-		_TurnAngle = -90;
+	else if (_OriginAngle == -90) {		//左
 		_OffsetX -= _MovementSpeed;
 		_X -= _MovementSpeed;
 	}
-	else if (nChar ==VK_UP){
-		_TurnAngle = 0;
+	else if (_OriginAngle == 0){			//上
 		_OffsetY -= _MovementSpeed;
 		_Y -= _MovementSpeed;
 	}
-	else if (nChar == VK_DOWN) {
-		_TurnAngle = 180;
+	else if (_OriginAngle == 180) {		//下
 		_OffsetY += _MovementSpeed;
 		_Y += _MovementSpeed;
-	}
-	if (_TurnAngle != _OriginAngle) {
-		LocationPoint(_OffsetX, _OffsetY);
-		_OriginAngle = _TurnAngle;
-		TurnFace();
 	}
 	Animation();
 }
 void CTank::Animation() {
-	int index;
-	index = _Tank.GetFrameIndexOfBitmap();
-	
-	for (int i = index; i < 2+index; i++){
-		for (int j = last_time; j < 10; j++){
-			if (clock() - last_time >= 10) {
-				last_time = clock();
-				_Tank.SetFrameIndexOfBitmap(i);
-				break;
-			}
-		}
+	if (_FrameTime%2==0){
+		_Tank.SetFrameIndexOfBitmap(_Frameindex + 1);
+		_Frameindex += 1;
 	}
+	else {
+		_Tank.SetFrameIndexOfBitmap(_Frameindex - 1);
+		_Frameindex -= 1;
+	}
+	_FrameTime += 1;
 }
-void CTank::TurnFace() {
-
-	if (_OriginAngle == 90){
-		_FrameIndex = 0;
-		_Tank.SetFrameIndexOfBitmap(0);
+void CTank::TurnFace(UINT nChar) {
+	if (nChar == VK_RIGHT) {			//右
+		_TurnAngle = 90;
 	}
-	else if (_OriginAngle == -90){
-		_FrameIndex = 2;
-		_Tank.SetFrameIndexOfBitmap(2);
+	else if (nChar == VK_LEFT) {		//左
+		_TurnAngle = -90;
 	}
-	else if (_OriginAngle == 0) {
-		_FrameIndex = 4;
-		_Tank.SetFrameIndexOfBitmap(4);
+	else if (nChar == VK_UP) {			//上
+		_TurnAngle = 0;
 	}
-	else if (_OriginAngle == 180){
-		_FrameIndex = 6;
-		_Tank.SetFrameIndexOfBitmap(6);
+	else if (nChar == VK_DOWN) {		//下
+		_TurnAngle = 180;
 	}
-	
+	if (_TurnAngle != _OriginAngle) {
+		LocationPoint(_OffsetX, _OffsetY);
+		_OriginAngle = _TurnAngle;
+		if (_OriginAngle == 90) {
+			_Frameindex = 0;
+		}
+		else if (_OriginAngle == -90) {
+			_Frameindex = 2;
+		}
+		else if (_OriginAngle == 0) {
+			_Frameindex = 4;
+		}
+		else if (_OriginAngle == 180) {
+			_Frameindex = 6;
+		}
+		_FrameTime= 0;
+	}
+	_Tank.SetFrameIndexOfBitmap(_Frameindex);
 }
 void CTank::LocationPoint(int _x, int _y) {
 	if (abs(_x) > _LocationDistance){
--- Source/Game/CTank.h
+++ Source/Game/CTank.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 namespace game_framework {
 	class CTank
 	{
@@ -7,31 +7,28 @@ namespace game_framework {
 		int GetX1();
 		int GetY1();
 		bool isBreak();
-		void Animation();
-		//void AnimationOnce();
-		void Attacke();
-		void LevelUP();
-		void LocationPoint(int x,int y);
-		void Move(UINT nChar);
-		void OnShow();
-		void SetXY(int x, int y);
-		void TurnFace();
+		void Animation();					
+		//void AnimationOnce();			
+		void Attacke();						//attack
+		void LevelUP();						//升級
+		void LocationPoint(int x,int y);	//位置校正
+		void Move(UINT nChar);				//移動
+		void OnShow();						//SHOW
+		void SetXY(int x, int y);			//座標設定
+		void TurnFace(UINT nChar);			//調整圖片方向
 	protected:
 		CMovingBitmap _Tank;
-		clock_t last_time;
-		int _X;
-		int _Y;
-		int _FrameIndex;
-		int _OriginAngle,_TurnAngle;
-		int _Level;
-		int _Life;
-		int _OffsetX, _OffsetY;
-		int _PointX, _PointY;
-		int _MovementSpeed;
-		int _LocationDistance;
-		bool _AttackSpeedUP;
-		bool _CanBreakIron;
-		bool _DoubleAttack;
+		int _X,_Y;							//地圖座標								
+		int _FrameTime,_Frameindex;			//動畫計時器,動畫初始禎
+		int _OriginAngle,_TurnAngle;		//原始角度,轉向角度
+		int _Level,_Life;					//坦克等級&生命
+		int _OffsetX, _OffsetY;				//偏移長度
+		int _PointX, _PointY;				//紀錄定位點
+		int _MovementSpeed;					//移動速度
+		int _LocationDistance;				//偏移長度上限
+		bool _AttackSpeedUP;				//功速提升		 level >= 2
+		bool _CanBreakIron;					//可破壞鐵牆		 level >= 4
+		bool _DoubleAttack;					//可發射兩發子彈   level >= 3
 	};
 
 }
