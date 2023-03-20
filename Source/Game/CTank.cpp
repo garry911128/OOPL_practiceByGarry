63989696ecdc6d3c8b33877443eade1a46f0c176
Gen-Yan Chen <t110590048@ntut.edu.tw>
2023-03-20 20:46:02 +0800

    finish

--- Source/Game/CTank.cpp
+++ Source/Game/CTank.cpp
@@ -11,9 +11,9 @@
 // Tank Parent
 using namespace game_framework;
 
-CTank::CTank():Width(32), Height(32) {
-	_X = Width*8+100;
-	_Y = Height*24;
+CTank::CTank() :Width(32), Height(32) {
+	_X = Width * 8 + 100;
+	_Y = Height * 24;
 	_Level = 1;								// 等級
 	_Life = 1;								// 生命
 	//_FrontX =0;								// 面對方向的前方格子X座標
@@ -24,17 +24,15 @@ CTank::CTank():Width(32), Height(32) {
 	_Frameindex = 4;						// 動畫偵 (0 is east,1 south, 2 west,3 north)*2
 	_FrameSecond = 3;						// 動畫變換速度
 	_LocationDistance = Height;				// 定位點距離
-	_OffsetX = 0;							// 偏移X
-	_OffsetY = 0;							// 偏移Y
-	_PointX = (_X - 100) / Width;			// 轉換定位X
-	_PointY = (_Y / Height);				// 轉換定位Y
 	_MovementSpeed = 2;						// 移動速度
 	_AttackSpeedUP = false;
 	_CanBreakIron = false;
 	_DoubleAttack = false;
-	_Tank.SetAnimation(1,true);
+	_Tank.SetAnimation(1, true);
 	_IfFire = false;
 	_FrontXY = { {0,0},{0,0} };
+	_NowGrid = { (_X-100) / Width, _Y / Height };
+	_OffsetXY = { 0,0 };
 	//_Bullet.LoadBitmap();
 }
 bool CTank::GetIfFire() {
@@ -75,31 +73,34 @@ void CTank::FireBullet() {
 
 void CTank::Move() {
 	if (_OriginAngle == Right){
-		_OffsetX += _MovementSpeed;
 		_X += _MovementSpeed;
+		_OffsetXY[0] += _MovementSpeed;
 	}
-	if (_OriginAngle == Left){
-		_OffsetX -= _MovementSpeed;
+	else if (_OriginAngle == Left){
 		_X -= _MovementSpeed;
+		_OffsetXY[0] -= _MovementSpeed;
 	}
-	if (_OriginAngle == Up){
-		_OffsetY -= _MovementSpeed;
+	else if (_OriginAngle == Up){
 		_Y -= _MovementSpeed;
+		_OffsetXY[1] -= _MovementSpeed;
 	}
-	if (_OriginAngle == Down) {
-		_OffsetY += _MovementSpeed;
+	else if (_OriginAngle == Down) {
 		_Y += _MovementSpeed;
+		_OffsetXY[1] += _MovementSpeed;
 	}
-	if (abs(_OffsetX) == _LocationDistance) {
-		if (_OffsetX > 0) _PointX += 1;
-		else _PointX -= 1;
+	for (int i = 0; i < 2; i++){
+		if (abs(_OffsetXY[i]) >= 32){		//當坦克持續移動到下一格時 偏移要歸零 不然_NowGrid會加太多次
+			_OffsetXY[i] = 0;
+		}
+		else if (abs(_OffsetXY[i]) == 8){	//當坦克移動超過8pixel 代表定位點要往下一格走
+			if (_OffsetXY[i] < 0){
+				_NowGrid[i] -= 1;
+			}
+			else if (_OffsetXY[i] > 0){
+				_NowGrid[i] += 1;
+			}
+		}
 	}
-	if (abs(_OffsetY) == _LocationDistance) {
-		if(_OffsetY>0) _PointY += 1;
-		else _PointY -= 1;
-	}
-	if (abs(_OffsetX) == Width * 2)_OffsetX = 0;
-	if (abs(_OffsetY) == Height * 2)_OffsetY = 0;
 }
 void CTank::TurnFace(UINT nChar) {
 	if (nChar == VK_RIGHT) {
@@ -115,7 +116,7 @@ void CTank::TurnFace(UINT nChar) {
 		_TurnAngle = Down;
 	}
 	if (_TurnAngle != _OriginAngle) {
-		LocationPoint(_OffsetX, _OffsetY);
+		LocationPoint();
 		_OriginAngle = _TurnAngle;
 		if (_OriginAngle == Right) {
 			_Frameindex = 0;
@@ -146,17 +147,10 @@ void CTank::Animation() {
 	}
 	_FrameTime += 1;
 }
-void CTank::LocationPoint(int _x, int _y) {
-	/*if (abs(_x) > _LocationDistance){
-		_PointX += _x / Width;
-	}
-	if (abs(_y) > _LocationDistance) {
-		_PointY += _y / Height;
-	}*/
-	_OffsetX = 0;
-	_OffsetY = 0;
-	SetXY(_PointX*Width+100, _PointY*Height);
-}
+void CTank::LocationPoint() {
+	SetXY( _NowGrid[0]*Width+100 , _NowGrid[1]*Height );
+	for (int i = 0; i < 2; i++) _OffsetXY[i] = 0;			//轉向後坦克的定位回正 偏移數值歸零
+} 
 
 void CTank::OnShow() {
 	_Tank.SetFrameIndexOfBitmap(_Frameindex);
--- Source/Game/CTank.h
+++ Source/Game/CTank.h
@@ -19,7 +19,7 @@ namespace game_framework {
 		void Animation();					
 		//void AnimationOnce();			
 		void LevelUP();						//升級
-		void LocationPoint(int x,int y);	//位置校正
+		void LocationPoint();				//位置校正
 		void Move();						//移動
 		void OnShow();						//SHOW
 		void SetXY(int x, int y);			//座標設定
@@ -48,19 +48,19 @@ namespace game_framework {
 		clock_t _Last_time; // last time that bullet move; (we can consider it as delay)
 		CBullet _Bullet;
 		CMovingBitmap _Tank;
-		int Width,Height;					//OneGrid寬,高
-		int _X,_Y;							//地圖座標								
-		int _FrameTime,_Frameindex,_FrameSecond;			//動畫計時器,動畫初始禎,計時秒數
-		int _OriginAngle,_TurnAngle;		//原始角度,轉向角度
-		int _Level,_Life;					//坦克等級&生命
-		int _OffsetX, _OffsetY;				//偏移長度
-		int _PointX, _PointY;				//紀錄定位點
-		int _MovementSpeed;					//移動速度
-		int _LocationDistance;				//偏移距離上限
-		vector<vector<int>> _FrontXY;		//前方的X和Y 
-		bool _AttackSpeedUP;				//功速提升		 level >= 2
-		bool _CanBreakIron;					//可破壞鐵牆		 level >= 4
-		bool _DoubleAttack;					//可發射兩發子彈   level >= 3
+		int Width,Height;							//OneGrid寬,高
+		int _X,_Y;									//地圖座標								
+		int _FrameTime,_Frameindex,_FrameSecond;	//動畫計時器,動畫初始禎,計時秒數
+		int _OriginAngle,_TurnAngle;				//原始角度,轉向角度
+		int _Level,_Life;							//坦克等級&生命
+		int _MovementSpeed;							//移動速度
+		int _LocationDistance;						//偏移距離上限
+		vector<vector<int>> _FrontXY;				//前方的X和Y 
+		vector<int> _NowGrid;						//現在的格子
+		vector<int> _OffsetXY;						//偏移距離
+		bool _AttackSpeedUP;						//功速提升		 level >= 2
+		bool _CanBreakIron;							//可破壞鐵牆		 level >= 4
+		bool _DoubleAttack;							//可發射兩發子彈   level >= 3
 	};
 
 }
